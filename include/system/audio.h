f88ac871eb2d5281f8dc9ea8ca002950f9ece0e9
Darren Smith <nerevar@gmail.com>
2015-01-23 22:43:07 -0600

    Fix compile warnings
    Change-Id: I490651378f01c68807d777dbd805c36828255988

--- include/system/audio.h
+++ include/system/audio.h
@@ -160,7 +160,7 @@ typedef enum {
  * It can also be used for certain formats to give informations not present in
  * the encoded audio stream (e.g. octet alignement for AMR).
  */
-typedef enum {
+enum {
     AUDIO_FORMAT_INVALID             = 0xFFFFFFFFUL,
     AUDIO_FORMAT_DEFAULT             = 0,
     AUDIO_FORMAT_PCM                 = 0x00000000UL, /* DO NOT CHANGE */
@@ -215,7 +215,9 @@ typedef enum {
                                         AUDIO_FORMAT_PCM_SUB_16_BIT),
     AUDIO_FORMAT_PCM_24_BIT_OFFLOAD  = (AUDIO_FORMAT_PCM_OFFLOAD |
                                         AUDIO_FORMAT_PCM_SUB_8_24_BIT),
-} audio_format_t;
+};
+
+typedef uint32_t audio_format_t;
 
 enum {
     /* output channels */
@@ -589,6 +591,15 @@ typedef struct {
 static const audio_offload_info_t AUDIO_INFO_INITIALIZER = {
     version: AUDIO_OFFLOAD_INFO_VERSION_CURRENT,
     size: sizeof(audio_offload_info_t),
+    sample_rate: 0,
+    channel_mask: 0,
+    format: AUDIO_FORMAT_INVALID,
+    stream_type: AUDIO_STREAM_DEFAULT,
+    bit_rate: 0,
+    duration_us: 0,
+    has_video: 0,
+    is_streaming: 0,
+    bit_width: 0
 };
 
 static inline bool audio_is_output_device(audio_devices_t device)
